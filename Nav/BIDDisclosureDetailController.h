5c508a09a1aec9d18c18ba94d53e294282f3855f
kolyjjj <me.koly.li@gmail.com>
2012-12-23 11:07:30 +0800

    Created a disclosureDetail controller which extends UIViewController

---
+++ Nav/BIDDisclosureDetailController.h
@@ -0,0 +1,13 @@
+//
+//  BIDDisclosureDetailController.h
+//  Nav
+//
+//  Created by koly on 12/23/12.
+//  Copyright (c) 2012 koly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDDisclosureDetailController : UIViewController
+
+@end
---
+++ Nav/BIDDisclosureDetailController.m
@@ -0,0 +1,38 @@
+//
+//  BIDDisclosureDetailController.m
+//  Nav
+//
+//  Created by koly on 12/23/12.
+//  Copyright (c) 2012 koly. All rights reserved.
+//
+
+#import "BIDDisclosureDetailController.h"
+
+@interface BIDDisclosureDetailController ()
+
+@end
+
+@implementation BIDDisclosureDetailController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
