7aaa34e56752dc91feea429dbc9e87164338a2be
kolyjjj <me.koly.li@gmail.com>
2012-12-23 11:06:00 +0800

    Created a disclosureButton controller which extends the SecondLevel controller

---
+++ Nav/BIDDisclosureButtonController.h
@@ -0,0 +1,13 @@
+//
+//  BIDDisclosureButtonController.h
+//  Nav
+//
+//  Created by koly on 12/23/12.
+//  Copyright (c) 2012 koly. All rights reserved.
+//
+
+#import "BIDSecondLevelViewController.h"
+
+@interface BIDDisclosureButtonController : BIDSecondLevelViewController
+
+@end
