4e79b550d73dcdb4bc7cc6a2a59a5cfdef83464c
kolyjjj <me.koly.li@gmail.com>
2012-12-23 10:36:48 +0800

    Created another tableview controller which holds an UIImage property

---
+++ Nav/BIDSecondLevelViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDSecondLevelViewController.h
+//  Nav
+//
+//  Created by koly on 12/23/12.
+//  Copyright (c) 2012 koly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDSecondLevelViewController : UITableViewController
+
+@end
