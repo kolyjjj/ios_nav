c244286e4222ad4e55ad9dd57b2ebf29ebaa5172
kolyjjj <me.koly.li@gmail.com>
2012-12-23 10:28:01 +0800

    Added a tableview controller with nib

---
+++ Nav/BIDFirstLevelController.h
@@ -0,0 +1,13 @@
+//
+//  BIDFirstLevelController.h
+//  Nav
+//
+//  Created by koly on 12/23/12.
+//  Copyright (c) 2012 koly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDFirstLevelController : UITableViewController
+
+@end
